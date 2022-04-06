dea08e120334d62cf8d9e983699cc111b4408e28
MadhupriyaGundeti <madhupriyareddy1919@gmail.com>
2022-04-06 13:45:41 -0500

    Update soup.js

--- routes/soup.js
+++ routes/soup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('soup', { title: 'Search Result soup' });
+  res.render('soup', { title: 'Search Results soup' });
 });
 
 module.exports = router;
